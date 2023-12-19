99b712a3b6cf27e00be498882411c0090ae0830f
Bhupendra-Gehlot1 <gehlotbhopesh007@gmail.com>
2023-12-19 15:31:42 +0530

    ivrs dil se final bhaii

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -12,6 +12,9 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import RightDrawer from "./RightDrawer";
 import DestinationScreen from "../screens/DestinationScreen";
 import StationScreen from "../screens/StationScreen";
+import { SelectLang, pnr } from "../drawer";
+import AnnouncementScreen from "../screens/AnnouncementScreen";
+import XTransaction from "../screens/XTransaction";
 
 // Main Function
 function AppNavigation() {
@@ -27,6 +30,10 @@ function AppNavigation() {
         <Stack.Screen name="Welcome" component={WelcomeScreen} />
         <Stack.Screen name="Destination" component={DestinationScreen} />
         <Stack.Screen name="Station" component={StationScreen} />
+        <Stack.Screen name="annoucement" component={AnnouncementScreen} />
+        <Stack.Screen name="Changelanguage" component={SelectLang} />
+        <Stack.Screen name="Xtransaction" component={XTransaction} />
+        <Stack.Screen name="Pnr" component={pnr} />
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
       </Stack.Navigator>
--- src/screens/IVRScreen.js
+++ src/screens/IVRScreen.js
@@ -5,12 +5,14 @@ import {
   SafeAreaView,
   Image,
   ScrollView,
+  Linking,
 } from "react-native";
 import React from "react";
 import TopBar from "../components/topBar";
 import { PREDEFINED_LANGUAGE } from "../constants/config";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { MapPinIcon, MicrophoneIcon } from "react-native-heroicons/solid";
+import { useNavigation } from "@react-navigation/native";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
@@ -18,6 +20,7 @@ import {
 
 const IVRScreen = () => {
   const [lang, setLang] = React.useState(null);
+  const navigation = useNavigation();
   React.useEffect(() => {
     const fetchData = async () => {
       try {
@@ -37,94 +40,78 @@ const IVRScreen = () => {
     <>
       <SafeAreaView className="flex-1 bg-white">
         <TopBar heading={PREDEFINED_LANGUAGE["IVR"][lang]} />
-        <View style={{ height: hp(50) }}>
+        <View style={{ height: hp(40) }}>
           <ScrollView className="flex-column m-4 p-0 item-center border-black border-2 rounded bg-slate-100">
             <View className="flex-row items-center justify-center my-1 mx-2 p-0">
               <Text className="text-xl p-1 font-bold">
                 Hello and welcome to RailBhasi
               </Text>
             </View>
-            <TouchableOpacity>
-              <View className="flex-row items-center justify-center border-blue-800 border-2 my-1 mx-2 rounded-lg p-0">
-                <Text className="text-base p-1 font-bold">
-                  Press 1 - Call the Emergency Medical Helpline
-                  {/* 138 */}
-                </Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <View className="flex-row items-center justify-center border-blue-800 border-2 my-1 mx-2 rounded-lg p-0">
-                <Text className="text-base p-1 font-bold">
-                  Press 2 - Call the Police Helpline
-                  {/* 182 */}
-                </Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <View className="flex-row items-center justify-center border-blue-800 border-2 my-1 mx-2 rounded-lg p-0">
-                <Text className="text-base p-1 font-bold">
-                  Press 3 - Check the PNR Information
-                  {/* navigateto pnr info */}
-                </Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <View className="flex-row items-center justify-center border-blue-800 border-2 my-1 mx-2 rounded-lg p-0">
-                <Text className="text-base p-1 font-bold">
-                  Press 4 - Check the Train Running Status
-                  {/* navigateto live train */}
-                </Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <View className="flex-row items-center justify-center border-blue-800 border-2 my-1 mx-2 rounded-lg p-0">
-                <Text className="text-base p-1 font-bold">
-                  Press 5 - Check the Station Information
-                  {/* navigateto station info */}
-                </Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <View className="flex-row items-center justify-center border-blue-800 border-2 my-1 mx-2 rounded-lg p-0">
-                <Text className="text-base p-1 font-bold">
-                  Press 6 - Register a complaint
-                  {/* 139 */}
-                </Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <View className="flex-row items-center justify-center border-blue-800 border-2 my-1 mx-2 rounded-lg p-0">
-                <Text className="text-base p-1 font-bold">
-                  Press 7 - Check the Refund and Cancellation Status
-                  {/* 139 */}
-                </Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <View className="flex-row items-center justify-center border-blue-800 border-2 my-1 mx-2 rounded-lg p-0">
-                <Text className="text-base p-1 font-bold">
-                  Press 8 - Check Special Services for Womens and Handicapped
-                  peoples
-                  {/* 139 */}
-                </Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <View className="flex-row items-center justify-center border-blue-800 border-2 my-1 mx-2 rounded-lg p-0">
-                <Text className="text-base p-1 font-bold">
-                  Press 9 - Change Current Language
-                  {/* App inbuild language change */}
-                </Text>
-              </View>
-            </TouchableOpacity>
+            <View className="flex-row items-center justify-center border-blue-800 border-2 my-1 mx-2 rounded-lg p-0">
+              <Text className="text-base p-1 font-bold">
+                Press 1 - Call the Emergency Medical Helpline
+                {/* 138 */}
+              </Text>
+            </View>
+            <View className="flex-row items-center justify-center border-blue-800 border-2 my-1 mx-2 rounded-lg p-0">
+              <Text className="text-base p-1 font-bold">
+                Press 2 - Call the Police Helpline
+                {/* 182 */}
+              </Text>
+            </View>
+            <View className="flex-row items-center justify-center border-blue-800 border-2 my-1 mx-2 rounded-lg p-0">
+              <Text className="text-base p-1 font-bold">
+                Press 3 - Check the PNR Information
+                {/* navigateto pnr info */}
+              </Text>
+            </View>
+            <View className="flex-row items-center justify-center border-blue-800 border-2 my-1 mx-2 rounded-lg p-0">
+              <Text className="text-base p-1 font-bold">
+                Press 4 - Get the Live station Annoucement and Station Informations
+                {/* navigateto annoucement train */}
+              </Text>
+            </View>
+            <View className="flex-row items-center justify-center border-blue-800 border-2 my-1 mx-2 rounded-lg p-0">
+              <Text className="text-base p-1 font-bold">
+              Press 5 - Check The Real-Time Railway Station Annoucements
+                {/* navigateto station info */}
+              </Text>
+            </View>
+            <View className="flex-row items-center justify-center border-blue-800 border-2 my-1 mx-2 rounded-lg p-0">
+              <Text className="text-base p-1 font-bold">
+                Press 6 - Register a complaint
+                {/* 139 */}
+              </Text>
+            </View>
+            <View className="flex-row items-center justify-center border-blue-800 border-2 my-1 mx-2 rounded-lg p-0">
+              <Text className="text-base p-1 font-bold">
+                Press 7 - Check the Refund and Cancellation Status
+                {/* 139 */}
+              </Text>
+            </View>
+            <View className="flex-row items-center justify-center border-blue-800 border-2 my-1 mx-2 rounded-lg p-0">
+              <Text className="text-base p-1 font-bold">
+                Press 8 - Check Special Services for Womens and Handicapped
+                peoples
+                {/* 139 */}
+              </Text>
+            </View>
+            <View className="flex-row items-center justify-center border-blue-800 border-2 mt-1 mb-4 mx-2 rounded-lg p-0">
+              <Text className="text-base p-1 font-bold">
+                Press 0 - Change Current Language
+                {/* App inbuild language change */}
+              </Text>
+            </View>
           </ScrollView>
         </View>
         <View className="flex-column items-center justify-center">
-          <View className="flex-column items-center p-2 mx-2 justify-between">
+          <View className="flex-column items-center p-2 mx-2 mb-2 justify-between">
             <View className="flex-row justify-start mx-1 p-1">
               <TouchableOpacity
-                className="p-3 mx-2 mb-1 rounded-xl bg-blue-500"
-                onPress={() => {}}
+                className="p-3 mx-2 rounded-xl bg-blue-500"
+                onPress={() => {
+                  Linking.openURL("tel:138");
+                }}
                 mode="elevated"
                 dark={true}
               >
@@ -133,8 +120,8 @@ const IVRScreen = () => {
                 </Text>
               </TouchableOpacity>
               <TouchableOpacity
-                className="p-3 mx-2 mb-1 rounded-xl bg-blue-500"
-                onPress={() => {}}
+                className="p-3 mx-2 rounded-xl bg-blue-500"
+                onPress={() => {Linking.openURL("tel:182");}}
                 mode="elevated"
                 dark={true}
               >
@@ -143,8 +130,8 @@ const IVRScreen = () => {
                 </Text>
               </TouchableOpacity>
               <TouchableOpacity
-                className="p-3 mx-2 mb-1 rounded-xl bg-blue-500"
-                onPress={() => {}}
+                className="p-3 mx-2 rounded-xl bg-blue-500"
+                onPress={() => navigation.navigate("Pnr")}
                 mode="elevated"
                 dark={true}
               >
@@ -155,8 +142,8 @@ const IVRScreen = () => {
             </View>
             <View className="flex-row justify-start mx-1 p-1">
               <TouchableOpacity
-                className="p-3 mx-2 mb-1 rounded-xl bg-blue-500"
-                onPress={() => {}}
+                className="p-3 mx-2 rounded-xl bg-blue-500"
+                onPress={() => navigation.navigate("annoucement")}
                 mode="elevated"
                 dark={true}
               >
@@ -165,8 +152,8 @@ const IVRScreen = () => {
                 </Text>
               </TouchableOpacity>
               <TouchableOpacity
-                className="p-3 mx-2 mb-1 rounded-xl bg-blue-500"
-                onPress={() => {}}
+                className="p-3 mx-2 rounded-xl bg-blue-500"
+                onPress={() => navigation.navigate("Xtransaction")}
                 mode="elevated"
                 dark={true}
               >
@@ -175,8 +162,8 @@ const IVRScreen = () => {
                 </Text>
               </TouchableOpacity>
               <TouchableOpacity
-                className="p-3 mx-2 mb-1 rounded-xl bg-blue-500"
-                onPress={() => {}}
+                className="p-3 mx-2 rounded-xl bg-blue-500"
+                onPress={() => {Linking.openURL("tel:139");}}
                 mode="elevated"
                 dark={true}
               >
@@ -187,8 +174,8 @@ const IVRScreen = () => {
             </View>
             <View className="flex-row justify-start mx-1 p-1">
               <TouchableOpacity
-                className="p-3 mx-2 mb-1 rounded-xl bg-blue-500"
-                onPress={() => {}}
+                className="p-3 mx-2 rounded-xl bg-blue-500"
+                onPress={() => {Linking.openURL("tel:139");}}
                 mode="elevated"
                 dark={true}
               >
@@ -197,8 +184,8 @@ const IVRScreen = () => {
                 </Text>
               </TouchableOpacity>
               <TouchableOpacity
-                className="p-3 mx-2 mb-1 rounded-xl bg-blue-500"
-                onPress={() => {}}
+                className="p-3 mx-2 rounded-xl bg-blue-500"
+                onPress={() => {Linking.openURL("tel:139");}}
                 mode="elevated"
                 dark={true}
               >
@@ -207,13 +194,45 @@ const IVRScreen = () => {
                 </Text>
               </TouchableOpacity>
               <TouchableOpacity
-                className="p-3 mx-2 mb-1 rounded-xl bg-blue-500"
+                className="p-3 mx-2 rounded-xl bg-blue-500"
                 onPress={() => {}}
                 mode="elevated"
                 dark={true}
               >
                 <Text className="text-center text-4xl text-white font-bold w-10 h-10">
                   9
+                </Text>
+              </TouchableOpacity>
+            </View>
+            <View className="flex-row justify-start mx-1 p-1">
+              <TouchableOpacity
+                className="p-3 mx-2 rounded-xl bg-blue-500"
+                onPress={() => {}}
+                mode="elevated"
+                dark={true}
+              >
+                <Text className="text-center text-4xl text-white font-bold w-10 h-10">
+                  *
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                className="p-3 mx-2 rounded-xl bg-blue-500"
+                onPress={() => navigation.navigate("Changelanguage")}
+                mode="elevated"
+                dark={true}
+              >
+                <Text className="text-center text-4xl text-white font-bold w-10 h-10">
+                  0
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                className="p-3 mx-2 rounded-xl bg-blue-500"
+                onPress={() => {}}
+                mode="elevated"
+                dark={true}
+              >
+                <Text className="text-center text-4xl text-white font-bold w-10 h-10">
+                  #
                 </Text>
               </TouchableOpacity>
             </View>
