7f27b6b029148d2fa11d1aa3497d01748d00cc0d
Aditya Prasad <84986092+adityaprasad-op@users.noreply.github.com>
2023-12-20 04:00:44 +0530

    Update IVRScreen.js

--- src/screens/IVRScreen.js
+++ src/screens/IVRScreen.js
@@ -5,19 +5,44 @@ import {
   SafeAreaView,
   Image,
   ScrollView,
+  Linking,
+  TextInput,
 } from "react-native";
 import React from "react";
 import TopBar from "../components/topBar";
 import { PREDEFINED_LANGUAGE } from "../constants/config";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { MapPinIcon, MicrophoneIcon } from "react-native-heroicons/solid";
+import { MapPinIcon, MicrophoneIcon, PhoneIcon } from "react-native-heroicons/solid";
+import { useNavigation } from "@react-navigation/native";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
 
 const IVRScreen = () => {
+  // const [customText, setCustomText] = React.useState('');
+  // const [showCustomText, setShowCustomText] = React.useState(false);
   const [lang, setLang] = React.useState(null);
+
+  const [inputText, setInputText] = React.useState('');
+  const handleNumericInput = (num) => {
+    setInputText(prevInput => prevInput + num);
+  };
+
+  // const handleOptionPress = (option) => {
+  //   switch (option) {
+  //     case '3':
+  //       setCustomText('Enter your Mobile Number');
+  //       setShowCustomText(true);
+  //       break;
+  //     default:
+  //       setCustomText('');
+  //       setShowCustomText(false);
+  //       break;
+  //   }
+  // };
+  
+  const navigation = useNavigation();
   React.useEffect(() => {
     const fetchData = async () => {
       try {
@@ -37,187 +62,158 @@ const IVRScreen = () => {
     <>
       <SafeAreaView className="flex-1 bg-white">
         <TopBar heading={PREDEFINED_LANGUAGE["IVR"][lang]} />
-        <View style={{ height: hp(50) }}>
-          <ScrollView className="flex-column m-4 p-0 item-center border-black border-2 rounded bg-slate-100">
+        <View className="flex-column items-center justify-center" style={{ height: hp(33) }}>
+        <Text style={{ fontSize: 20, fontWeight: 'bold', marginTop: 200 }}>{inputText}</Text>
+
+          {/* <ScrollView className="flex-column m-4 p-0 item-center border-black border-2 rounded bg-slate-100">
+          {showCustomText && (
+            <View style={{ alignItems: 'center' }}>
+            <Text style={{ fontSize: 18, fontWeight: 'bold', textAlign: 'center' }}>
+              {customText}
+            </Text>
+            <View style={{ width: '80%' }}>
+            <TextInput
+                  style={{ borderWidth: 1, borderColor: 'gray', padding: 8, borderRadius: 5 }}
+                  placeholder="Enter your input"
+                  keyboardType="numeric"
+                />
+              </View>
+            </View>
+          )}
+          {!showCustomText && (
+          <View>
             <View className="flex-row items-center justify-center my-1 mx-2 p-0">
               <Text className="text-xl p-1 font-bold">
                 Hello and welcome to RailBhasi
               </Text>
             </View>
-            <TouchableOpacity>
-              <View className="flex-row items-center justify-center border-blue-800 border-2 my-1 mx-2 rounded-lg p-0">
-                <Text className="text-base p-1 font-bold">
-                  Press 1 - Call the Emergency Medical Helpline
-                  {/* 138 */}
-                </Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <View className="flex-row items-center justify-center border-blue-800 border-2 my-1 mx-2 rounded-lg p-0">
-                <Text className="text-base p-1 font-bold">
-                  Press 2 - Call the Police Helpline
-                  {/* 182 */}
-                </Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <View className="flex-row items-center justify-center border-blue-800 border-2 my-1 mx-2 rounded-lg p-0">
-                <Text className="text-base p-1 font-bold">
-                  Press 3 - Check the PNR Information
-                  {/* navigateto pnr info */}
-                </Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <View className="flex-row items-center justify-center border-blue-800 border-2 my-1 mx-2 rounded-lg p-0">
-                <Text className="text-base p-1 font-bold">
-                  Press 4 - Check the Train Running Status
-                  {/* navigateto live train */}
-                </Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <View className="flex-row items-center justify-center border-blue-800 border-2 my-1 mx-2 rounded-lg p-0">
-                <Text className="text-base p-1 font-bold">
-                  Press 5 - Check the Station Information
-                  {/* navigateto station info */}
-                </Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <View className="flex-row items-center justify-center border-blue-800 border-2 my-1 mx-2 rounded-lg p-0">
-                <Text className="text-base p-1 font-bold">
-                  Press 6 - Register a complaint
-                  {/* 139 */}
-                </Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <View className="flex-row items-center justify-center border-blue-800 border-2 my-1 mx-2 rounded-lg p-0">
-                <Text className="text-base p-1 font-bold">
-                  Press 7 - Check the Refund and Cancellation Status
-                  {/* 139 */}
-                </Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <View className="flex-row items-center justify-center border-blue-800 border-2 my-1 mx-2 rounded-lg p-0">
-                <Text className="text-base p-1 font-bold">
-                  Press 8 - Check Special Services for Womens and Handicapped
-                  peoples
-                  {/* 139 */}
-                </Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <View className="flex-row items-center justify-center border-blue-800 border-2 my-1 mx-2 rounded-lg p-0">
-                <Text className="text-base p-1 font-bold">
-                  Press 9 - Change Current Language
-                  {/* App inbuild language change */}
-                </Text>
-              </View>
-            </TouchableOpacity>
-          </ScrollView>
+            <View className="flex-row items-center justify-center border-blue-800 border-2 my-1 mx-2 rounded-lg p-0">
+              <Text style={{ fontSize: 16, fontWeight: 'bold' }}>
+                Press 1- Call the medical emergency helpline
+              </Text>
+            </View>
+            <View className="flex-row items-center justify-center border-blue-800 border-2 my-1 mx-2 rounded-lg p-0">
+              <Text className="text-base p-1 font-bold">
+                Press 2 - Call the Police Helpline
+              </Text>
+            </View>
+            <View className="flex-row items-center justify-center border-blue-800 border-2 my-1 mx-2 rounded-lg p-0">
+              <Text style={{ fontSize: 16, padding: 8, fontWeight: 'bold' }}>
+                Press 3 - Check the PNR Information
+              </Text>
+            </View>
+            <View className="flex-row items-center justify-center border-blue-800 border-2 my-1 mx-2 rounded-lg p-0">
+              <Text className="text-base p-1 font-bold">
+                Press 4 - Get the Live station Annoucement and Station Informations
+              </Text>
+            </View>
+            <View className="flex-row items-center justify-center border-blue-800 border-2 my-1 mx-2 rounded-lg p-0">
+              <Text className="text-base p-1 font-bold">
+              Press 5 - Check The Real-Time Railway Station Annoucements
+              </Text>
+            </View>
+            <View className="flex-row items-center justify-center border-blue-800 border-2 my-1 mx-2 rounded-lg p-0">
+              <Text className="text-base p-1 font-bold">
+                Press 6 - Register a complaint
+              </Text>
+            </View>
+            <View className="flex-row items-center justify-center border-blue-800 border-2 my-1 mx-2 rounded-lg p-0">
+              <Text className="text-base p-1 font-bold">
+                Press 7 - Check the Refund and Cancellation Status
+              </Text>
+            </View>
+            <View className="flex-row items-center justify-center border-blue-800 border-2 my-1 mx-2 rounded-lg p-0">
+              <Text className="text-base p-1 font-bold">
+                Press 8 - Check Special Services for Womens and Handicapped
+                peoples
+              </Text>
+            </View>
+            <View className="flex-row items-center justify-center border-blue-800 border-2 mt-1 mb-4 mx-2 rounded-lg p-0">
+              <Text className="text-base p-1 font-bold">
+                Press 0 - Change Current Language
+              </Text>
+            </View>
+          </View>
+          )}
+          </ScrollView> */}
         </View>
+        
         <View className="flex-column items-center justify-center">
-          <View className="flex-column items-center p-2 mx-2 justify-between">
+          <View className="flex-column items-center p-2 mx-2 mb-2 justify-between">
             <View className="flex-row justify-start mx-1 p-1">
-              <TouchableOpacity
-                className="p-3 mx-2 mb-1 rounded-xl bg-blue-500"
-                onPress={() => {}}
-                mode="elevated"
-                dark={true}
-              >
+            <TouchableOpacity className="p-3 mx-2 rounded-xl bg-blue-500" onPress={() => handleNumericInput('1')}>
                 <Text className="text-center text-4xl text-white font-bold w-10 h-10">
                   1
                 </Text>
               </TouchableOpacity>
-              <TouchableOpacity
-                className="p-3 mx-2 mb-1 rounded-xl bg-blue-500"
-                onPress={() => {}}
-                mode="elevated"
-                dark={true}
-              >
+              <TouchableOpacity className="p-3 mx-2 rounded-xl bg-blue-500" onPress={() => handleNumericInput('2')}>
                 <Text className="text-center text-4xl text-white font-bold w-10 h-10">
                   2
                 </Text>
               </TouchableOpacity>
-              <TouchableOpacity
-                className="p-3 mx-2 mb-1 rounded-xl bg-blue-500"
-                onPress={() => {}}
-                mode="elevated"
-                dark={true}
-              >
+              <TouchableOpacity className="p-3 mx-2 rounded-xl bg-blue-500" onPress={() => handleNumericInput('3')}>
                 <Text className="text-center text-4xl text-white font-bold w-10 h-10">
                   3
                 </Text>
               </TouchableOpacity>
             </View>
             <View className="flex-row justify-start mx-1 p-1">
-              <TouchableOpacity
-                className="p-3 mx-2 mb-1 rounded-xl bg-blue-500"
-                onPress={() => {}}
-                mode="elevated"
-                dark={true}
-              >
+            <TouchableOpacity className="p-3 mx-2 rounded-xl bg-blue-500" onPress={() => handleNumericInput('4')}>
                 <Text className="text-center text-4xl text-white font-bold w-10 h-10">
                   4
                 </Text>
               </TouchableOpacity>
-              <TouchableOpacity
-                className="p-3 mx-2 mb-1 rounded-xl bg-blue-500"
-                onPress={() => {}}
-                mode="elevated"
-                dark={true}
-              >
+              <TouchableOpacity className="p-3 mx-2 rounded-xl bg-blue-500" onPress={() => handleNumericInput('5')}>
                 <Text className="text-center text-4xl text-white font-bold w-10 h-10">
                   5
                 </Text>
               </TouchableOpacity>
-              <TouchableOpacity
-                className="p-3 mx-2 mb-1 rounded-xl bg-blue-500"
-                onPress={() => {}}
-                mode="elevated"
-                dark={true}
-              >
+              <TouchableOpacity className="p-3 mx-2 rounded-xl bg-blue-500" onPress={() => handleNumericInput('6')}>
                 <Text className="text-center text-4xl text-white font-bold w-10 h-10">
                   6
                 </Text>
               </TouchableOpacity>
             </View>
             <View className="flex-row justify-start mx-1 p-1">
-              <TouchableOpacity
-                className="p-3 mx-2 mb-1 rounded-xl bg-blue-500"
-                onPress={() => {}}
-                mode="elevated"
-                dark={true}
-              >
+            <TouchableOpacity className="p-3 mx-2 rounded-xl bg-blue-500" onPress={() => handleNumericInput('7')}>
                 <Text className="text-center text-4xl text-white font-bold w-10 h-10">
                   7
                 </Text>
               </TouchableOpacity>
-              <TouchableOpacity
-                className="p-3 mx-2 mb-1 rounded-xl bg-blue-500"
-                onPress={() => {}}
-                mode="elevated"
-                dark={true}
-              >
+              <TouchableOpacity className="p-3 mx-2 rounded-xl bg-blue-500" onPress={() => handleNumericInput('8')}>
                 <Text className="text-center text-4xl text-white font-bold w-10 h-10">
                   8
                 </Text>
               </TouchableOpacity>
-              <TouchableOpacity
-                className="p-3 mx-2 mb-1 rounded-xl bg-blue-500"
-                onPress={() => {}}
-                mode="elevated"
-                dark={true}
-              >
+              <TouchableOpacity className="p-3 mx-2 rounded-xl bg-blue-500" onPress={() => handleNumericInput('9')}>
                 <Text className="text-center text-4xl text-white font-bold w-10 h-10">
                   9
                 </Text>
               </TouchableOpacity>
             </View>
+            <View className="flex-row justify-start mx-1 p-1">
+            <TouchableOpacity className="p-3 mx-2 rounded-xl bg-blue-500" onPress={() => handleNumericInput('*')}>
+                <Text className="text-center text-4xl text-white font-bold w-10 h-10">
+                  *
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity className="p-3 mx-2 rounded-xl bg-blue-500" onPress={() => handleNumericInput('0')}>
+                <Text className="text-center text-4xl text-white font-bold w-10 h-10">
+                  0
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity className="p-3 mx-2 rounded-xl bg-blue-500" onPress={() => handleNumericInput('#')}>
+                <Text className="text-center text-4xl text-white font-bold w-15 h-15">
+                  #
+                </Text>
+              </TouchableOpacity>
+            </View>
           </View>
+        </View>
+        <View className="flex-column items-center justify-center">
+        <TouchableOpacity style={{width: wp(17) , height: wp(17), borderRadius: 400, paddingTop: 18, paddingLeft:20 }} className="p-3 mx-2 bg-green-500" onPress={() => handleNumericInput('#')}>
+                  <PhoneIcon size={35} color="#fff"/>
+              </TouchableOpacity>
         </View>
       </SafeAreaView>
     </>
